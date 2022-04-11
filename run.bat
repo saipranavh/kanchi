9af7d8446b2cbe54bf8d70d19d271ff4bcc23096
saik <sahishonline415@gmail.com>
2022-04-11 20:57:45 +0530

    aaa

--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-pytest -v -s -m "sanity and regression" --html=./Reports/report.html testCases/ --browser chrome
-pytest -v -s -m "sanity " --html=./Reports/report.html testCases/ --browser edge
+    pytest -v -s -m "sanity and regression" --html=./Reports/report.html testCases/ --browser chrome
